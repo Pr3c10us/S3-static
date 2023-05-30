a1b6e0c5b42e91a6bbee78dc7ef26e6b6b58862a
Owolabi precious <owo.pre.eno@gmail.com>
2023-05-30 19:15:40 +0100

    removed private zone

--- s3_static_website/data.tf
+++ s3_static_website/data.tf
@@ -1,6 +1,7 @@
 data "aws_route53_zone" "domain_data" {
   name         = var.cert.cert_1.domain
-  private_zone = false
+  # private_zone = false
+  # vpc_id = 
   depends_on = [
     aws_route53_zone.website-hosted-zone,
   ]
