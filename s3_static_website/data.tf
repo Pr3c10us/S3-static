2a89e245e5ce20c2b1b0afce8f538dbfc77f1264
Owolabi precious <owo.pre.eno@gmail.com>
2023-05-30 19:25:03 +0100

    commened out vpc

--- s3_static_website/data.tf
+++ s3_static_website/data.tf
@@ -1,7 +1,7 @@
 data "aws_route53_zone" "domain_data" {
   name         = "newName"
   private_zone = false
-  vpc_id = 
+  # vpc_id = 
   depends_on = [
     aws_route53_zone.website-hosted-zone,
   ]
