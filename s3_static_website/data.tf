ac8000b92f20d2f947528e8be7a193e34bd0612b
Owolabi precious <owo.pre.eno@gmail.com>
2023-05-30 18:38:46 +0100

    array

--- s3_static_website/data.tf
+++ s3_static_website/data.tf
@@ -1,5 +1,5 @@
 data "aws_route53_zone" "domain_data" {
-  name         = var.cert.cert_1.domain
+  name         = var.cert.cert_1.domain[0]
   private_zone = false
   depends_on = [
     aws_route53_zone.website-hosted-zone,
