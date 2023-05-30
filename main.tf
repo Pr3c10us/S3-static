c75361acbbd905d489ce1faeae41f130a24f1138
Abdul-Bakri <66844942+Bakri-10@users.noreply.github.com>
2023-05-30 16:33:57 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,42 +29,6 @@ resource "aws_route53_zone" "example" {
   name = "opeluther001.com"  # Replace with your desired domain name
 }
 
-# Create an ACM certificate
-resource "aws_acm_certificate" "example" {
-  domain_name       = "opeluther001.com"  # Replace with your domain name
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-# Convert domain_validation_options set to a map
-locals {
-  domain_validation_options_map = { for option in aws_acm_certificate.example.domain_validation_options : option.domain_name => option }
-}
-
-# Create the ACM certificate validation records
-resource "aws_route53_record" "acm_validation" {
-  for_each = local.domain_validation_options_map
-
-  zone_id = aws_route53_zone.example.zone_id
-  name    = each.value.resource_record_name
-  type    = each.value.resource_record_type
-  ttl     = 300
-  records = [each.value.resource_record_value]
-
-  depends_on = [aws_route53_zone.example, aws_acm_certificate.example]
-}
-
-# Wait for the ACM certificate validation to complete
-resource "aws_acm_certificate_validation" "example" {
-  certificate_arn         = aws_acm_certificate.example.arn
-  validation_record_fqdns = [for record in values(local.domain_validation_options_map) : aws_route53_record.acm_validation[record.domain_name].fqdn]
-
-  depends_on = [aws_route53_record.acm_validation]
-}
-
 # Create the CloudFront distribution for the S3 bucket
 resource "aws_cloudfront_distribution" "website" {
   origin {
@@ -83,7 +47,7 @@ resource "aws_cloudfront_distribution" "website" {
         forward = "none"
       }
     }
-    viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "allow-all"  # Update to redirect-to-http
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
@@ -95,11 +59,6 @@ resource "aws_cloudfront_distribution" "website" {
     geo_restriction {
       restriction_type = "none"
     }
-  }
-
-  viewer_certificate {
-    acm_certificate_arn = aws_acm_certificate.example.arn
-    ssl_support_method  = "sni-only"
   }
 
   aliases = ["opeluther001.com"]  # Replace with your desired domain name
@@ -128,8 +87,4 @@ output "domain_name" {
 
 output "zone_id" {
   value = aws_route53_zone.example.zone_id
-}
-
-output "acm_certificate_arn" {
-  value = aws_acm_certificate.example.arn
 }
