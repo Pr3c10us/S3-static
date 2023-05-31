1b368077d0e9f5ca6182f899a31833be00444967
Abdul-Bakri <66844942+Bakri-10@users.noreply.github.com>
2023-05-31 01:08:05 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -90,22 +90,25 @@ resource "aws_s3_bucket_website_configuration" "s3_demo_bucket" {
 resource "aws_s3_bucket_policy" "s3_demo_bucket" {
   bucket = aws_s3_bucket.s3_demo_bucket.id
 
-  policy = jsonencode({
-    Version   = "2012-10-17",
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject",
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = "s3:GetObject",
-        Resource  = [
-          aws_s3_bucket.s3_demo_bucket.arn,
-          "${aws_s3_bucket.s3_demo_bucket.arn}/*",
-        ],
-      },
-    ],
-  })
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadGetObject",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": [
+        "${aws_s3_bucket.s3_demo_bucket.arn}",
+        "${aws_s3_bucket.s3_demo_bucket.arn}/*"
+      ]
+    }
+  ]
 }
+EOF
+}
+
 
 resource "aws_route53_zone" "website-hosted-zone" {
   name = var.domain_name
