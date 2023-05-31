0e7d70eb2cc921a9b64db1a44ffae763a7f46705
Abdul-Bakri <66844942+Bakri-10@users.noreply.github.com>
2023-05-30 22:31:21 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -114,20 +114,18 @@ resource "namedotcom_domain_nameservers" "eaaladejana-live" {
 
 resource "aws_s3_bucket_policy" "example-policy" {
   bucket = aws_s3_bucket.website.id
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "PublicReadGetObject",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::${aws_s3_bucket.website.arn}/*"
-    }
-  ]
-}
-POLICY
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "PublicReadGetObject",
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": "s3:GetObject",
+        "Resource": "${aws_s3_bucket.website.arn}/*"
+      }
+    ]
+  })
 }
 
 resource "aws_cloudfront_distribution" "website" {
