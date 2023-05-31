44d6672c7cb393ff72ca6cabeed137fa109e1251
Abdul-Bakri <66844942+Bakri-10@users.noreply.github.com>
2023-05-31 01:00:24 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,6 +49,31 @@ resource "aws_s3_bucket" "s3_demo_bucket" {
   bucket = var.bucket_name
 
 }
+resource "aws_s3_bucket_ownership_controls" "s3_demo_bucket" {
+  bucket = aws_s3_bucket.s3_demo_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_demo_bucket" {
+  bucket = aws_s3_bucket.s3_demo_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "s3_demo_bucket" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.s3_demo_bucket.id
+  acl    = "public-read"
+}
 
 resource "aws_s3_bucket_website_configuration" "s3_demo_bucket" {
   bucket = aws_s3_bucket.s3_demo_bucket.id
@@ -60,12 +85,6 @@ resource "aws_s3_bucket_website_configuration" "s3_demo_bucket" {
   error_document {
     key = "error.html"
   }
-}
-
-resource "aws_s3_bucket_acl" "s3_demo_bucket" {
-  bucket = aws_s3_bucket.s3_demo_bucket.id
-
-  acl = "public-read"
 }
 
 resource "aws_s3_bucket_policy" "s3_demo_bucket" {
